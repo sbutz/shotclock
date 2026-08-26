55248a2020a305a867091a649df95a1260d998c9
Stefan Butz <mail@butz.st>
2026-08-26 19:53:08 +0200

    Use improved 6s sound

--- src/Sounds.ts
+++ src/Sounds.ts
@@ -1,14 +1,14 @@
 const beep = new Audio(process.env.PUBLIC_URL + "/shotclock.mp3");
-const beep5s = new Audio(process.env.PUBLIC_URL + "/shotclock_5s.mp3");
+const beep6s = new Audio(process.env.PUBLIC_URL + "/shotclock_6s.mp3");
 const time = new Audio(process.env.PUBLIC_URL + "/time.mp3");
 
 export function startBeep() {
-  beep5s.play();
+  beep6s.play();
 }
 
 export function stopBeep() {
-  beep5s.pause();
-  beep5s.currentTime = 0;
+  beep6s.pause();
+  beep6s.currentTime = 0;
 }
 
 async function playBeep() {
